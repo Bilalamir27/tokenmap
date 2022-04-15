9d6c4f35f7c16504f422085290a8e1d0e65ff708
Syed Bilal Amir <amirsye@sheirdancollege.ca>
2022-04-15 16:28:15 -0400

    added a firestore_function file added the corresponding addUser.dart added models file which will have the user which contains needed fields edited main dart

---
+++ lib/models/user.dart
@@ -0,0 +1,17 @@
+
+class User{
+  String uid;
+  String firstName;
+  String lastName;
+  String email;
+  String password;
+
+  User({
+    required this.uid,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password
+  });
+
+}
