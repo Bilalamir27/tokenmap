7462c200f1d5d9e2f4cad2367234fb7124ec21a0
Syed Bilal Amir <amirsye@sheirdancollege.ca>
2022-04-17 18:47:08 -0400

    added and fixed firebase information

--- lib/Views/signup.dart
+++ lib/Views/signup.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SigninPage extends StatefulWidget {
-  SigninPage({Key? key, required this.title}) : super(key: key);
+  SigninPage({Key? key, required this.title, required this.uid}) : super(key: key);
   final String title;
-
+  String uid;
   @override
   _SigninPageState createState() => _SigninPageState();
 }
 
 class _SigninPageState extends State<SigninPage> {
+
+  final _auth = FirebaseAuth.instance;
+  String email,password,name;
+
   @override
   Widget build(BuildContext context) {
 
@@ -25,7 +31,7 @@ class _SigninPageState extends State<SigninPage> {
             ),
           ],
         ),
-      ), 
+      ),
     );
   }
 }
