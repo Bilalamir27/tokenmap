c703655d83f0f7a2763a5b4af457b28fe40bd72a
Haashim Chouhdry <chouhdry@sheridancollege.ca>
2022-04-18 04:19:16 -0400

    created the ui for login page

--- lib/Views/login.dart
+++ lib/Views/login.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tokenmap/Views/navigation.dart';
+
+import 'signup.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key, required this.title}) : super(key: key);
@@ -9,26 +14,126 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final _auth = FirebaseAuth.instance;
+  var email;
+  var password;
+  var name;
+
+  _signIn(){
+    //https://api.flutter.dev/flutter/material/AlertDialog-class.html
+    showDialog(context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Unable to sign in"),
+            content: Text("Username or password incorrect"),
+            actions: <Widget>[
+              ElevatedButton(onPressed: () => Navigator.of(context).pop(),
+                  child: Text("OK"))
+            ],
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const TextField(
-                  decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
+    return new Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Opacity(
+                opacity: 0.8,
+                child: Image.asset(
+                  'assets/background.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-        ]),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(color: Colors.transparent,height: 20),
+                  Flexible(
+                      child: Container(
+                        margin: const EdgeInsets.all(10),
+                        child:TextFormField(
+                          onChanged: (x){
+                            email=x;
+                          },
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              hintStyle: TextStyle(color: Colors.white),
+                              hintText: "Enter Email",
+                              filled: true,
+                              fillColor: Colors.black.withOpacity(0.3),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide(color: Colors.blue)
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide(color: Colors.blue)
+
+                              )
+
+                          ),
+                        ) ,
+                      )),
+                  Flexible(
+                    child:Container(
+                      margin: const EdgeInsets.all(10),
+                      child:TextFormField(
+                        onChanged: (x){
+                          password=x;
+                        },
+                        obscureText: true,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            hintStyle: TextStyle(color: Colors.white),
+                            hintText: "Enter Password",
+                            filled: true,
+                            fillColor: Colors.black.withOpacity(0.3),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide(color: Colors.blue)
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide(color: Colors.blue)
+
+                            )
+
+                        ),
+                      ) ,
+                    ),),
+                  Flexible(
+                      child:ElevatedButton(
+                        onPressed: () async{
+                          try{
+                            final result = await _auth.signInWithEmailAndPassword(email: email.toString().trim(), password: password.toString().trim());
+                            // if(result != null){
+                            //   Navigator.push(context, MaterialPageRoute(
+                            //       //builder: (context) => _signIn(uid: result.user.uid)));
+                            // }
+                          }catch(e){
+                            _signIn();
+                          }
+                        },
+                        //color: Colors.deepOrangeAccent,
+                        child: Text("LOGIN",style: TextStyle(color: Colors.white,fontSize: 20)),
+                        //color: Colors.black12,
+                      ) )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
